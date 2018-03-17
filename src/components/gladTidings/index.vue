63c437afc5a84fe4999d2c5d3443762ffa0c3d04
aigle <840207473@qq.com>
2018-03-17 15:56:41 +0800

    confllcit

--- src/components/gladTidings/index.vue
+++ src/components/gladTidings/index.vue
@@ -1,63 +1,71 @@
 <template>
-    <div>
-        <router-link to="/collectReport">收房报备</router-link>
-        <router-link to="/rentReport">租房报备</router-link>
-        <router-link to="/transferReport">调房报备</router-link>
-        <router-link to="/continueCollect">续收报备</router-link>
+  <div>
+    <router-link to="/collectReport">
+      <van-button size="large"> 收房报备</van-button>
+    </router-link>
+    <router-link to="/rentReport">
+      <van-button size="large">租房报备</van-button>
+    </router-link>
+    <router-link to="/transferReport">
+      <van-button size="large">调房报备</van-button>
+    </router-link>
+    <router-link to="/continueCollect">
+      <van-button size="large">续收报备</van-button>
+    </router-link>
 
-      <router-link to="/unCollectBeforeRent">
-        <van-button size="large">
-          未收先租
-        </van-button>
-      </router-link>
-      <router-link to="/continueRent">
-        <van-button size="large">
-          续租报备
-        </van-button>
-      </router-link>
+    <router-link to="/unCollectBeforeRent">
+      <van-button size="large">
+        未收先租
+      </van-button>
+    </router-link>
+    <router-link to="/continueRent">
+      <van-button size="large">
+        续租报备
+      </van-button>
+    </router-link>
 
-      <router-link to="/drawback">
-        <van-button size="large">
-          退款报备
-        </van-button>
-      </router-link>
-      <router-link to="/friedBill">
-        <van-button size="large">
-          炸单报备
-        </van-button>
-      </router-link>
-      <router-link to="/agencyRent">
-        <van-button size="large">
-          中介费报备
-        </van-button>
-      </router-link>
-      <router-link to="/special">
-        <van-button size="large">
-          特殊事项报备
-        </van-button>
-      </router-link>
-      <router-link to="/confiscate">
-        <van-button size="large">
-          充公报备
-        </van-button>
-      </router-link>
-      <router-link to="/finalPayment">
-        <van-button size="large">
-          尾款房租报备
-        </van-button>
-      </router-link>
-    </div>
+    <router-link to="/drawback">
+      <van-button size="large">
+        退款报备
+      </van-button>
+    </router-link>
+    <router-link to="/friedBill">
+      <van-button size="large">
+        炸单报备
+      </van-button>
+    </router-link>
+    <router-link to="/agencyRent">
+      <van-button size="large">
+        中介费报备
+      </van-button>
+    </router-link>
+    <router-link to="/special">
+      <van-button size="large">
+        特殊事项报备
+      </van-button>
+    </router-link>
+    <router-link to="/confiscate">
+      <van-button size="large">
+        充公报备
+      </van-button>
+    </router-link>
+    <router-link to="/finalPayment">
+      <van-button size="large">
+        尾款房租报备
+      </van-button>
+    </router-link>
+  </div>
 </template>
 
 <script>
-    export default {
-        name: 'hello',
-        data () {
-            return {
-                msg: 'Welcome to Your Vue.js App'
-            }
-        }
+  export default {
+    name: 'hello',
+    data() {
+      return {
+        msg: 'Welcome to Your Vue.js App'
+      }
     }
+  }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
