9af4496417ceaf16d4088a5726816030471a46eb
lin60126 <601267015@qq.com>
2018-04-12 19:34:27 +0800

    fdfs

--- src/components/examine/writings/index.vue
+++ src/components/examine/writings/index.vue
@@ -145,7 +145,6 @@
         let Agents = ["Android", "iPhone", "SymbianOS", "Windows Phone", "iPod"];
         let flag = true;
         for (let v = 0; v < Agents.length; v++) {
-          alert(Agents[v]);
           if (userAgentInfo.indexOf(Agents[v]) > 0) {
             flag = false;
             break;
