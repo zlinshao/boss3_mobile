2cfafd038f04b179603982beb4ff51c968bc54f2
lin60126 <601267015@qq.com>
2018-04-19 14:53:54 +0800

    fdfs

--- src/components/examine/warning/index.vue
+++ src/components/examine/warning/index.vue
@@ -47,7 +47,9 @@
           <p v-if="key.uri.indexOf('.xls') > -1" class="a2"></p>
           <p v-if="key.uri.indexOf('.txt') > -1" class="a3"></p>
           <p v-if="key.uri.indexOf('.pdf') > -1" class="a4"></p>
-          <p v-if="key.uri.indexOf('.pptx') > -1 || key.uri.indexOf('.ppt') > -1 || key.uri.indexOf('.jpg') > -1 || key.uri.indexOf('.png') > -1" class="a5"></p>
+          <p
+            v-if="key.uri.indexOf('.pptx') > -1 || key.uri.indexOf('.ppt') > -1 || key.uri.indexOf('.jpg') > -1 || key.uri.indexOf('.png') > -1"
+            class="a5"></p>
           <span>{{key.display_name}}</span>
         </a>
       </div>
@@ -73,11 +75,7 @@
     beforeRouteEnter(to, from, next) {
       next(vm => {
         vm.path = from.path;
-        if (from.path === '/') {
-          vm.corp();
-        } else {
-          vm.search();
-        }
+        vm.search();
       })
     },
     mounted() {
@@ -85,7 +83,6 @@
       this.recall = false;
       this.recall1 = false;
     },
-    watch: {},
     methods: {
       search() {
         this.warningList(this.ids);
@@ -108,155 +105,6 @@
             this.recall1 = false;
             this.titles = '此消息消失不见了';
           }
-        })
-      },
-      corp() {
-        let that = this;
-        this.$http.get(this.urls + 'special/special/dingConfig').then((res) => {
-          let _config = res.data;
-          DingTalkPC.runtime.permission.requestAuthCode({
-            corpId: _config.corpId,
-            onSuccess: function (info) {
-              that.$http.get(that.urls + 'special/special/userInfo', {
-                params: {
-                  'code': info.code,
-                }
-              }).then((res) => {
-                if (res.data.status !== 'fail') {
-                  if (res.data !== false) {
-                    let data = {};
-                    data.id = res.data.id;
-                    data.name = res.data.name;
-                    data.avatar = res.data.avatar;
-                    data.phone = res.data.phone;
-                    data.department_name = res.data.org[0].name;
-                    data.department_id = res.data.org[0].id;
-                    // data.display_name = res.data.role[0].display_name;
-                    sessionStorage.setItem('personal', JSON.stringify(data));
-                    globalConfig.personal = data;
-
-                    that.$http.post(that.address + 'oauth/token', {
-                      client_secret: globalConfig.client_secret,
-                      client_id: globalConfig.client_id,
-                      grant_type: 'password',
-                      username: res.data.phone,
-                      password: res.data.code,
-                    }).then((res) => {
-                      sessionStorage.setItem('myData', JSON.stringify(res.data.data));
-                      let head = res.data.data;
-                      globalConfig.header.Authorization = head.token_type + ' ' + head.access_token;
-                      that.search();
-                    });
-                  }
-                } else {
-                  DingTalkPC.device.notification.alert({
-                    message: "您不在系统内，请联系管理员添加！",
-                    title: "提示信息",
-                    buttonName: "关闭",
-                    onSuccess: function () {
-                    },
-                    onFail: function (err) {
-                    }
-                  });
-                  dd.biz.navigation.close({
-                    onSuccess: function (result) {
-                    },
-                    onFail: function (err) {
-                    }
-                  });
-                }
-              })
-            },
-            onFail: function (err) {
-              DingTalkPC.device.notification.alert({
-                message: "您不在系统内，请联系管理员添加！",
-                title: "提示信息",
-                buttonName: "关闭",
-                onSuccess: function () {
-                },
-                onFail: function (err) {
-                }
-              });
-            }
-          });
-
-          dd.ready(function () {
-            dd.runtime.permission.requestAuthCode({
-              corpId: _config.corpId,
-              onSuccess: function (info) {
-                that.$http.get(that.urls + 'special/special/userInfo', {
-                  params: {
-                    'code': info.code,
-                  }
-                }).then((res) => {
-                  if (res.data.status !== 'fail') {
-                    if (res.data !== false) {
-                      let data = {};
-                      data.id = res.data.id;
-                      data.name = res.data.name;
-                      data.avatar = res.data.avatar;
-                      data.phone = res.data.phone;
-                      data.department_name = res.data.org[0].name;
-                      data.department_id = res.data.org[0].id;
-                      // data.display_name = res.data.role[0].display_name;
-                      sessionStorage.setItem('personal', JSON.stringify(data));
-                      globalConfig.personal = data;
-                      that.$http.post(that.address + 'oauth/token', {
-                        client_secret: globalConfig.client_secret,
-                        client_id: globalConfig.client_id,
-                        grant_type: 'password',
-                        username: res.data.phone,
-                        password: res.data.code,
-                      }).then((res) => {
-                        sessionStorage.setItem('myData', JSON.stringify(res.data.data));
-                        let head = res.data.data;
-                        globalConfig.header.Authorization = head.token_type + ' ' + head.access_token;
-                        that.search();
-                      });
-                    } else {
-                      setTimeout(() => {
-                        alert('请求超时请稍后再试');
-                        dd.biz.navigation.close({
-                          onSuccess: function (result) {
-                          },
-                          onFail: function (err) {
-                          }
-                        });
-                      }, 3000);
-                    }
-                  } else {
-                    alert('您不在系统内，请联系管理员添加！');
-                    dd.biz.navigation.close({
-                      onSuccess: function (result) {
-                      },
-                      onFail: function (err) {
-                      }
-                    });
-                  }
-                })
-              },
-              onFail: function (err) {
-                alert('您不在系统内，请联系管理员添加！');
-                dd.biz.navigation.close({
-                  onSuccess: function (result) {
-                  },
-                  onFail: function (err) {
-                  }
-                });
-              }
-            });
-            // 钉钉头部右侧
-            dd.biz.navigation.setRight({
-              show: false,
-              onSuccess: function (result) {
-              },
-              onFail: function (err) {
-              }
-            });
-          });
-          dd.error(function (err) {
-            alert('dd error: ' + JSON.stringify(err));
-          });
         })
       },
     },
