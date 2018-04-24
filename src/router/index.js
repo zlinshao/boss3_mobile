c58f04ba91b71922ae00e755042b71d738d9374d
lin60126 <601267015@qq.com>
2018-04-24 15:09:35 +0800

    fdfs

--- src/router/index.js
+++ src/router/index.js
@@ -42,7 +42,7 @@ import CollectHouse from '../components/gladTidings/collectHouse.vue'       //�
 import Organize from '../components/gladTidings/organize.vue'               //开单人搜索
 import Depart from '../components/common/selectDepartment.vue'              //部门搜索
 
-import ProductControlCenter from '../components/gladTidings/productControlCenter/index.vue'
+// import ProductControlCenter from '../components/gladTidings/productControlCenter/index.vue'
 
 
 Vue.use(Router);
@@ -269,14 +269,14 @@ export default new Router({
       component: FinalPayment
     },
 
-    {
-      path: '/productControlCenter',
-      name: '产品管控中心',
-      hidden: 'glad',
-      icon: 'iconfont icon-chanpinguankong',
-      meta: {title: '产品管控中心'},
-      component: ProductControlCenter
-    },
+    // {
+    //   path: '/productControlCenter',
+    //   name: '产品管控中心',
+    //   hidden: 'glad',
+    //   icon: 'iconfont icon-chanpinguankong',
+    //   meta: {title: '产品管控中心'},
+    //   component: ProductControlCenter
+    // },
     // {
     //   path: '/expense',
     //   hidden: 'examine',
