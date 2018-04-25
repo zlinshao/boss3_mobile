8e659a353b7bc14fce054d9592da8a8af5fda866
lin60126 <601267015@qq.com>
2018-04-25 17:18:41 +0800

    ffdfd

--- src/App.vue
+++ src/App.vue
@@ -58,7 +58,6 @@
           return response;
         }, function (error) {
           if (error && error.response) {
-            alert(JSON.stringify(error.response));
             if (error.response.status === 500) {
               alert('服务器故障,请联系产品经理');
               DingTalkPC.device.notification.alert({
