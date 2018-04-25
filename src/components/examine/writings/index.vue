b65f9a85f7167f6065a79b47e0ad83b47345043d
lin60126 <601267015@qq.com>
2018-04-25 18:52:03 +0800

    fdfds

--- src/components/examine/writings/index.vue
+++ src/components/examine/writings/index.vue
@@ -16,7 +16,7 @@
         <div v-for="key in cover_pic">
           <img v-for="p in key" :src="p.uri">
         </div>
-        <div class="post_text ql-editor" v-html="myData.content">
+        <div class="post_text ql-editor" id="content" v-html="myData.content">
 
         </div>
         <h6></h6>
@@ -300,29 +300,6 @@
 
 <style lang="scss">
   #writings {
-    .post_text.ql-editor{
-      h1 {
-        font-size: 32px;
-      }
-      h2 {
-        font-size: 24px;
-      }
-      h3 {
-        font-size: 18px;
-      }
-      h4 {
-        font-size: 16px;
-      }
-      h5 {
-        font-size: 13px;
-      }
-      h6 {
-        font-size: 12px;
-      }
-    }
-    b, strong, h1, h2, h3, h4, h5, h6 {
-      font-weight: bold;
-    }
     @mixin flex {
       display: flex;
       display: -webkit-flex;
@@ -464,6 +441,29 @@
       margin-top: .3rem;
       padding: .3rem;
       background: #FFFFFF;
+      #content {
+        h1 {
+          font-size: 32px;
+        }
+        h2 {
+          font-size: 24px;
+        }
+        h3 {
+          font-size: 18px;
+        }
+        h4 {
+          font-size: 16px;
+        }
+        h5 {
+          font-size: 13px;
+        }
+        h6 {
+          font-size: 12px;
+        }
+      }
+      b, strong, h1, h2, h3, h4, h5, h6 {
+        font-weight: bold;
+      }
     }
 
     .titles {
