715d0c94116d782697edf79cb27a63dbc23e101e
aigle <840207473@qq.com>
2018-04-28 14:51:40 +0800

    dd

--- src/components/common/UPLOAD.vue
+++ src/components/common/UPLOAD.vue
@@ -1,147 +1,77 @@
 <template>
   <div>
     <div id="container">
-
       <div :id="'pickfiles'+ID" class="pickfiles">
-        <div class="imgItem" v-for="(val,key) in editImg" v-if="editImg.length > 0">
-          <div style=" position: relative;">
-            <img v-if="val.is_video" src="../../assets/video.jpg" style="width: 1.5rem; height: 1.5rem;">
-            <img :src="val.uri" style="width: 1.5rem; height: 1.5rem;" v-else>
-            <div class="progress"><b style="color: #fff !important;"></b></div>
-            <div class="remove pic_delete van-icon van-icon-close" @click="deleteImage(key)">
-            </div>
-          </div>
-        </div>
-        <div class="upButton" @click="getTokenMessage" :id="ID">
+        <div class="upButton" :id="ID">
           <span class="plus">+</span>
         </div>
       </div>
+
     </div>
+
   </div>
 </template>
 
 <script>
-  import fileImage from '../../assets/video.jpg'
-  import {Dialog} from 'vant';
+
   export default {
     name: 'hello',
-    props: ['ID', 'editImage', 'isClear'],
-    data() {
+    props: ['ID'],
+    data () {
       return {
-
         imgArray: [],
         imgId: [],
         isUploading: false,
         activeIndex: null,
         uploader: null,
-        editImg: [],
-        token: '',
-        isStatus: true,
-        hhh: {},
       }
     },
-    mounted() {
-      this.active();
-      this.isStatus = true;
-    },
-    activated() {
-      this.isStatus = true;
-    },
-    watch: {
-      editImage: {
-        deep: true,
-        handler(val, old) {
-          this.editImg = this.editImage;
-          this.imgId = [];
-          for (let i = 0; i < val.length; i++) {
-            this.imgId.push(val[i].id)
+    mounted(){
+      let _this = this;
+      $(document).on('click', '.pic_delete', function () {
+        let id = $(this).attr("data-val");
+        let toremove = '';
+        for (let i in _this.uploader.files) {
+          if (_this.uploader.files[i].id === id) {
+            toremove = i;
           }
         }
-      },
-      isClear(val) {
-        if (val) {
-          this.imgId = [];
-          this.imgArray = [];
-          this.editImg = [];
+        $('#' + id).remove();
+        _this.uploader.splice(toremove, 1);
+        for (let i = 0; i < _this.imgArray.length; i++) {
+          if (_this.imgArray[i].indexOf(id) > -1) {
+            _this.imgArray.splice(i, 1)
+            _this.imgId.splice(i, 1)
+            _this.$emit('getImg', [_this.ID,_this.imgId, _this.isUploading]);
+          }
         }
-      }
+      });
+
+      this.getTokenMessage();
+      setInterval(()=>{
+        this.uploader.refresh();
+      },1000)
     },
     methods: {
-      active() {
-        let _this = this;
-        $(document).on('click', '#pickfiles' + this.ID + ' ' + '.pic_delete', function () {
-          let id = $(this).attr("data-val");
-          let toremove = '';
-          for (let i in _this.uploader.files) {
-            if (_this.uploader.files[i].id === id) {
-              toremove = i;
-            }
-          }
-          $('#' + id).remove();
-          _this.uploader.splice(toremove, 1);
-          for (let i = 0; i < _this.imgArray.length; i++) {
-            if (_this.imgArray[i].name.indexOf(id) > -1) {
 
-              _this.imgId.forEach((item) => {
-                if (_this.imgArray[i].id === item) {
-                  _this.imgId = _this.imgId.filter((x) => {
-                    return x !== item
-                  })
-                }
-              });
-
-              _this.imgArray.splice(i, 1);
-            }
-          }
-          _this.$emit('getImg', [_this.ID, _this.imgId, _this.isUploading]);
-        });
-        this.getTokenMessage();
-        setInterval(() => {
-          if (_this.uploader) {
-            this.uploader.refresh();
-          }
-        }, 1000)
-      },
-
-      deleteImage(key) {
-        this.imgId.splice(key, 1);
-        this.editImg.splice(key, 1);
-
-        // this.imgId = this.imgId.filter((x) => {
-        //   return x !== key
-        // });
-        // this.$emit('getImg', [this.ID, this.imgId, this.isUploading]);
-        // let imgObject = {};
-        // for (let img in this.editImg) {
-        //   if (img !== key) {
-        //     imgObject[img] = this.editImg[img];
-        //   }
-        // }
-        // this.editImg = {};
-        // this.editImg = imgObject;
-      },
       getTokenMessage() {
-        this.$http.get(globalConfig.server_user + 'files').then((res) => {
-          this.token = res.data.data;
-          if (this.isStatus) {
-            this.uploaderReady(res.data.data);
-          }
-          // this.uploaderReady(res.data.data);
+        this.$http.get(globalConfig.server_user + 'api/v1/files').then((res) => {
+          this.uploaderReady(res.data.data);
         })
       },
       uploaderReady(token) {
-        this.isStatus = false;
         let _this = this;
         _this.uploader = Qiniu.uploader({
           runtimes: 'html5,flash,html4',      // 上传模式，依次退化
-          browse_button: _this.ID,     //上传按钮的ID
-          uptoken: _this.token,                  // uptoken是上传凭证，由其他程序生成
+          browse_button: _this.ID  ,     //上传按钮的ID
+          uptoken: token,                     // uptoken是上传凭证，由其他程序生成
 
-          get_new_uptoken: true,             // 设置上传文件的时候是否每次都重新获取新的uptoken
+          get_new_uptoken: false,             // 设置上传文件的时候是否每次都重新获取新的uptoken
           unique_names: true,                 // 默认false，key为文件
-          domain: globalConfig.domain,  // bucket域名，下载资源时用到，必需
+          domain: 'http://static.lejias.cn',  // bucket域名，下载资源时用到，必需
 
-          max_file_size: '100mb',               // 最大文件体积限制
+//          container: 'container',             // 上传区域DOM ID，默认是browser_button的父元素
+          max_file_size: '100mb',             // 最大文件体积限制
           flash_swf_url: 'path/of/plupload/Moxie.swf',  //引入flash，相对路径
           max_retries: 1,                     // 上传失败最大重试次数
           dragdrop: true,                     // 开启可拖曳上传
@@ -151,17 +81,14 @@
 
           init: {
             'FilesAdded': function (up, files) {
-              _this.isUploading = true;
-              _this.$emit('getImg', [_this.ID, _this.imgId, _this.isUploading]);
-
               plupload.each(files, function (file) {
                 if (!file || !/image\//.test(file.type) || /photoshop/.test(file.type)) {
 
-                  $('#pickfiles' + _this.ID).prepend(`
+                  $('#pickfiles'+_this.ID).prepend(`
                     <div class="imgItem" id="${file.id}">
                       <div style=" width: 1.5rem;  height: 1.5rem; position: relative;">
-                        <img src="${fileImage}" style="width: 1.5rem; height: 1.5rem; ">
-                        <div class="progress"><b style="color: #aaa !important;"></b></div>
+                        <img src="" style="width: 1.5rem; height: 1.5rem; ">
+                        <div class="progress"><b></b></div>
                         <div class="remove pic_delete van-icon van-icon-close"  data-val=${file.id}>
 
                         </div>
@@ -173,7 +100,7 @@
 
                   fr.onload = function () {
 //                     文件添加进队列后，处理相关的事情
-                    $('#pickfiles' + _this.ID).prepend(`
+                    $('#pickfiles'+_this.ID).prepend(`
                     <div class="imgItem" id="${file.id}">
                       <div style=" position: relative;">
                         <img src="${fr.result}" style="width: 1.5rem; height: 1.5rem; ">
@@ -194,57 +121,50 @@
               _this.isUploading = true;
             },
             'UploadProgress': function (up, file) {
-
               // 每个文件上传时，处理相关的事情
               if (document.getElementById(file.id)) {
 
-                if (file.percent < 100) {
+                if(file.percent<100){
                   document.getElementById(file.id).getElementsByTagName('b')[0].innerHTML = '<span>' + file.percent + "%</span>";
-                } else {
+                }else {
                   document.getElementById(file.id).getElementsByTagName('b')[0].innerHTML = '<span class="van-icon van-icon-passed"></span>';
                 }
-
               }
+
             },
             'FileUploaded': function (up, file, info) {
+
+
+              // 每个文件上传成功后，处理相关的事情
+              // 其中info是文件上传成功后，服务端返回的json，形式如：
+              // {
+              //    "hash": "Fh8xVqod2MQ1mocfI4S4KpRL6D98",
+              //    "key": "gogopher.jpg"
+              //  }
+              // 获取url路径 传入后台保存到数据库
               let domain = up.getOption('domain');
               let url = JSON.parse(info);
               let sourceLink = domain + "/" + url.key;
 
-//              _this.isUpId = file.id;
-
-              _this.$http.post(globalConfig.server_user + 'files', {
+              _this.$http.post(globalConfig.server_user + 'api/v1/files', {
                 url: sourceLink,
-                name: url.key,
-                raw_name: file.name,
-                type: file.type,
-                size: file.size
+                name: url.key
               }).then((res) => {
                 if (res.data.status === "success") {
+//                  $('#'+file.id).remove();
                   _this.imgId.push(res.data.data.id);
-
-                  let object = {};
-                  object.id = res.data.data.id;
-                  object.name = res.data.data.name;
-                  _this.imgArray.push(object);
+                  _this.imgArray.push(res.data.data.name);
                   _this.$emit('getImg', [_this.ID, _this.imgId, _this.isUploading]);
                 }
               })
             },
+//            Error(up, err, errTip) {
+//              console.log(errTip);
+//            },
             'UploadComplete': function () {
               //队列文件处理完毕后，处理相关的事情
               _this.isUploading = false;
             },
-            'Error': function (up, err, errTip) {// 每个文件上传失败后,处理相关的事情
-              if (err.file && err.file.size !== undefined && err.file.size > 104857600) {
-                Dialog.alert({
-                  message: '文件最大不能超过100MB'
-                }).then(() => {
-                  // on close
-                });
-              }
-            }
-
 //            'Key': function (up, file) {
 //              // 若想在前端对每个文件的key进行个性化处理，可以配置该函数
 //              // 该配置必须要在unique_names: false，save_key: false时才生效
@@ -255,6 +175,7 @@
           }
         });
       },
+
     }
   }
 
