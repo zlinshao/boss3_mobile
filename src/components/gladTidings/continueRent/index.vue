e672965b85932b80fab45c572ba273c7817646f7
lin60126 <601267015@qq.com>
2018-05-23 00:08:07 +0800

    fdfs

--- src/components/gladTidings/continueRent/index.vue
+++ src/components/gladTidings/continueRent/index.vue
@@ -890,16 +890,22 @@
             this.form.deposit = draft.deposit;
             this.form.discount = draft.discount;
 
-            if (draft.receipt.length !== 0) {
-              this.amountReceipt = draft.receipt.length;
-              this.form.receipt = [];
-              for (let i = 0; i < draft.receipt.length; i++) {
-                this.form.receipt.push(draft.receipt[i]);
+            if (typeof draft.receipt !== "string") {
+              if (draft.receipt.length !== 0) {
+                this.amountReceipt = draft.receipt.length;
+                this.form.receipt = [];
+                for (let i = 0; i < draft.receipt.length; i++) {
+                  this.form.receipt.push(draft.receipt[i]);
+                }
+              } else {
+                this.amountReceipt = 1;
+                this.form.receipt = [];
+                this.form.receipt[0] = this.receiptDate;
               }
             } else {
               this.amountReceipt = 1;
               this.form.receipt = [];
-              this.form.receipt[0] = this.receiptDate;
+              this.form.receipt[0] = draft.receipt;
             }
 
 
