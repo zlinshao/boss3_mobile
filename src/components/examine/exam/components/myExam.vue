8988f79d96d9fec7e9829250b832d737985f8462
cj_jing <1429407967@qq.com>
2018-05-26 13:23:58 +0800

    'exam'

---
+++ src/components/examine/exam/components/myExam.vue
@@ -0,0 +1,109 @@
+<template>
+  <div id="myNaire">
+    <div class="container">
+      <div class="content" v-for="item in examData">
+        <div class="top">
+          <div style="float: left;"><img :src="personal.avatar" style="width: 50px;height: 50px;border-radius: 50%;">
+          </div>
+          <div style="width: 65%;float: left;margin-left: 20px;line-height: 35px;margin-top: -6px;">
+            <p style="color: #333;font-size: 20px;">{{personal.name}}</p>
+            <p style="font-size: 16px;">{{personal.department_name}}</p>
+          </div>
+          <div style="float: right;margin-right: 0px;margin-top: -23px;color: #39b1ff;">
+            <span v-if="item.available">进行中</span>
+            <span style="color: #e4393c;" v-else>已结束</span>
+          </div>
+        </div>
+        <div class="info">
+          <p>考试时间 <span>{{item.start_time}}</span></p>
+          <p>考试名称 <span>{{item.name}}</span></p>
+          <p>考试时长 <span>{{item.duration}} </span>分钟</p>
+        </div>
+        <div style="position: relative;text-align: center;">
+          <van-button class="view_history" @click="goDetail(item.id)">查看考试结果</van-button>
+        </div>
+        <div style="position: relative;margin-top: 20px;width: 100%;height: 20px;background: #eee;"></div>
+      </div>
+    </div>
+
+  </div>
+
+</template>
+
+<script>
+  export default {
+    name: "before-naire",
+    data() {
+      return {
+        showType: 'first',
+        personal: {},
+        examData: {},
+      };
+    },
+    mounted() {
+      this.personal = JSON.parse(sessionStorage.personal);
+      this.getExamData();
+    },
+    methods: {
+      goDetail(val) {
+        // this.$router.push({path: '/lookExam', query: {id: val}});
+      },
+      getExamData() {
+        this.$http.get(globalConfig.server + 'exam/exam/my?enrolled=1').then((res) => {
+          if (res.data.code === '30000') {
+            this.examData = res.data.data.data;
+            if (res.data.data.data.length < 1) {
+              this.examData = [];
+            }
+          } else {
+            this.examData = [];
+          }
+        });
+      },
+    }
+  };
+</script>
+
+<style lang="scss" scoped>
+  .container {
+    background: #fff;
+    color: #999;
+    width: 100%;
+    height: 100%;
+    .content {
+      background: #fff;
+      width: 100%;
+      height: 100%;
+      .top{
+        border-top:1px solid #ebebeb;
+      }
+      .top {
+        position: relative;
+        padding: 15px 20px;
+        border-bottom: 1px solid #ebebeb;
+        min-height: 80px;
+        margin-right: 0;
+      }
+      .info {
+        position: relative;
+        padding: 10px 70px;
+        margin-left: 20px;
+        line-height: 30px;
+        border-bottom: 1px solid #ebebeb;
+        span {
+          color: #393939;
+          margin-left: 10px;
+        }
+      }
+      .view_history {
+        position: relative;
+        padding: 0px 30px;
+        background: #39b1ff;
+        color: #fff;
+        margin-top: 20px;
+      }
+    }
+
+  }
+
+</style>
