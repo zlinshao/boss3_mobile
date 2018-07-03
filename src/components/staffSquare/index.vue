68bfc3425178292858375254987ed22bf58abe3f
cj_jing <1429407967@qq.com>
2018-07-03 16:15:56 +0800

    'swipe'

--- src/components/staffSquare/index.vue
+++ src/components/staffSquare/index.vue
@@ -13,7 +13,7 @@
               <img v-for="p in pic" :src="p.uri" width="100%" height="140" @click="goArticleDetail(image.id)"
                    style="min-height: 140px;"/>
             </span>
-            <div style="position: absolute;z-index: 1;background: rgba(0,0,0,0.3);width: 100%;left: 0;top: 95px;">
+            <div style="position: absolute;z-index: 1;background: rgba(0,0,0,0.3);width: 100%;left: 0;top: 96px;">
               <span
                 style="color: #FFFFFF;display: inline-block;height: 25px;line-height: 25px;margin: 10px 20px;width: 70%;overflow: hidden;white-space: nowrap;text-overflow: ellipsis;word-break: break-all;">{{image.title}}</span>
             </div>
