1f14894ba2a019c91d6d8a7a643cdd17f92419fb
lin60126 <601267015@qq.com>
2018-07-09 23:32:20 +0800

    upload

--- src/components/common/uploadIOS.vue
+++ src/components/common/uploadIOS.vue
@@ -21,7 +21,8 @@
     watch: {},
     methods: {
       tirggerFile(event) {
-        this.file = event.target.files; // (利用console.log输出看结构就知道如何处理档案资料)
+        this.file = event.target; // (利用console.log输出看结构就知道如何处理档案资料)
+        console.log(event.target.files)
       },
     },
   }
