cdcea2710ae15856acf03f1955f0f4aa318d4f73
lin60126 <601267015@qq.com>
2018-07-13 10:54:44 +0800

    photo

--- src/components/common/UPLOAD.vue
+++ src/components/common/UPLOAD.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="uploadContainer">
     <div id="container">
-      {{fileLength}}---{{imgId}}
+      {{fileLength}}==={{imgId}}==={{imgId.length}}
       <div :id="'pickfiles'+ID" class="pickfiles">
         <div class="imgItem" v-for="(val,key) in editImg" v-if="editImg.length > 0">
           <div style="position: relative; margin: .3rem 0 0 .3rem;">
