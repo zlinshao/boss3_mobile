366af5b61bcdfbdb21891adc432ee8d050d5e109
lin60126 <601267015@qq.com>
2018-07-19 18:53:30 +0800

    upload

--- src/components/common/UPLOAD.vue
+++ src/components/common/UPLOAD.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="uploadContainer">
     <div id="container">
-      222222222
+      333333
       <div :id="'pickfiles'+ID" class="pickfiles">
         <div class="imgItem" v-for="(val,key) in editImg" v-if="editImg.length > 0">
           <div style="position: relative; margin: .3rem 0 0 .3rem;">
@@ -86,7 +86,7 @@
         });
         $(document).on('click', '#pickfiles' + this.ID + ' ' + '.pic_delete', function () {
           let id = $(this).attr("data-val");
-          let span = $(this).prev().children('b').children('span');
+          let span = $(this).prev().children('b').children('span').attr('class');
           console.log(span);
           let close = false;
           if (span !== undefined) {
