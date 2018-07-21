76c94cfaf08dd5ab45d67b34356c6dc5521dd7c0
lin60126 <601267015@qq.com>
2018-07-21 16:19:57 +0800

    td

--- src/components/examine/dataStatic/index.vue
+++ src/components/examine/dataStatic/index.vue
@@ -176,144 +176,186 @@
           }
         });
       },
-
-      drawing(data) {
-        let Shape = F2.Shape;
-        let G = F2.G;
-        let Util = F2.Util;
-        let Global = F2.Global;
-        let Vector2 = G.Vector2;
-        Shape.registerShape('interval', 'tick', {
-          draw: function draw(cfg, container) {
-            let points = this.parsePoints(cfg.points);
-            let style = Util.mix({
-              stroke: cfg.color
-            }, Global.shape.interval, cfg.style);
-            if (cfg.isInCircle) {
-              let newPoints = points.slice(0);
-              if (this._coord.transposed) {
-                newPoints = [points[0], points[3], points[2], points[1]];
-              }
-
-              let _cfg$center = cfg.center,
-                x = _cfg$center.x,
-                y = _cfg$center.y;
-
-              let v = [1, 0];
-              let v0 = [newPoints[0].x - x, newPoints[0].y - y];
-              let v1 = [newPoints[1].x - x, newPoints[1].y - y];
-              let v2 = [newPoints[2].x - x, newPoints[2].y - y];
-
-              let startAngle = Vector2.angleTo(v, v1);
-              let endAngle = Vector2.angleTo(v, v2);
-              let r0 = Vector2.length(v0);
-              let r = Vector2.length(v1);
-
-              if (startAngle >= 1.5 * Math.PI) {
-                startAngle = startAngle - 2 * Math.PI;
-              }
-
-              if (endAngle >= 1.5 * Math.PI) {
-                endAngle = endAngle - 2 * Math.PI;
-              }
-
-              let lineWidth = r - r0;
-              let newRadius = r - lineWidth / 2;
-
-              return container.addShape('Arc', {
-                className: 'interval',
-                attrs: Util.mix({
-                  x: x,
-                  y: y,
-                  startAngle: startAngle,
-                  endAngle: endAngle,
-                  r: newRadius,
-                  lineWidth: lineWidth,
-                  lineCap: 'round'
-                }, style)
-              });
-            }
-          }
-        });
-        this.initialise(Util, data);
-      },
-
-      initialise(Util, data) {
+      drawing(val) {
         this.chart = new F2.Chart({
           id: 'mountNode',
-          width: this.screenWidth,
-          pixelRatio: window.devicePixelRatio,
+          pixelRatio: window.devicePixelRatio
         });
-        this.chart.source(this.data, {
+        this.chart.source(data, {
           y: {
             max: 100,
             min: 0
           }
         });
-        this.chart.axis(false); // hide all axis 坐标
-        this.chart.tooltip(false); // hide tooltip 个性化配置
-
-        // set polar coordinate 设置坐标系类型
-        this.chart.coord('polar', {  //极坐标系
+        this.chart.axis(false);
+        this.chart.tooltip(false);
+        this.chart.coord('polar', {
           transposed: true,
-          innerRadius: 0.9,
-          radius: 1
+          innerRadius: 0.8,
+          radius: 0.85
         });
-
-        // draw a cricle 辅助弧线
         this.chart.guide().arc({
-          start: [1, 0],
+          start: [0, 0],
           end: [1, 99.98],
           top: false,
           style: {
-            lineWidth: 15,
+            lineWidth: 20,
             stroke: '#ccc'
           }
         });
-
-        // 辅助 html
-        this.chart.guide().html({
+        this.chart.guide().text({
           position: ['50%', '50%'],
-          html: '<p id="number" style="font-size: 32px;margin: 0;color: #1890ff;transform: translate(-50%, -40%);"></p>',
-        });
-        // 使用矩形或者弧形，用面积来表示大小关系的图形，一般构成柱状图、饼图等图表
-        this.chart.interval().position('x*y').size(15).shape('tick').animate({
-          appear: {
-            duration: 1200,
-            easing: 'cubicIn',
-            animation: function animation(shape, animateCfg) {
-              let startAngle = shape.attr('startAngle');
-              let endAngle = shape.attr('endAngle');
-              shape.attr('endAngle', startAngle);
-              shape.animate().to(Util.mix({
-                attrs: {
-                  endAngle: endAngle,
-                }
-              }, animateCfg)).onUpdate(function (frame) {
-                if (data) {
-                  $('#number').text(Math.floor(frame * data) + '%');
-                } else {
-                  $('#number').text(0 + '%');
-                }
-              });
-            }
+          content: '85%',
+          style: {
+            fontSize: 24,
+            fill: '#1890FF'
           }
         });
-        this.chart.guide().text({
-          top: true, // 指定 guide 是否绘制在 canvas 最上层，默认为 true, 即绘制在最上层
-          position: [0, 0], // 文本的起始位置，值为原始数据值，支持 callback
-          content: '个人业绩占比(%)', // 显示的文本内容
-          style: {
-            fill: '#666', // 文本颜色
-            fontSize: '12', // 文本大小
-            fontWeight: 'bold', // 文本粗细
-            rotate: 0 // 旋转角度
-          }, // 文本的图形样式属性
-          offsetX: this.screenWidth / 3, // x 方向的偏移量
-          offsetY: 40, // y 方向偏移量
+        this.chart.interval().position('x*y').size(20).animate({
+          appear: {
+            duration: 1200,
+            easing: 'cubicIn'
+          }
         });
         this.chart.render();
-      },
+      }
+      // drawing(data) {
+      //   let Shape = F2.Shape;
+      //   let G = F2.G;
+      //   let Util = F2.Util;
+      //   let Global = F2.Global;
+      //   let Vector2 = G.Vector2;
+      //   Shape.registerShape('interval', 'tick', {
+      //     draw: function draw(cfg, container) {
+      //       let points = this.parsePoints(cfg.points);
+      //       let style = Util.mix({
+      //         stroke: cfg.color
+      //       }, Global.shape.interval, cfg.style);
+      //       if (cfg.isInCircle) {
+      //         let newPoints = points.slice(0);
+      //         if (this._coord.transposed) {
+      //           newPoints = [points[0], points[3], points[2], points[1]];
+      //         }
+      //
+      //         let _cfg$center = cfg.center,
+      //           x = _cfg$center.x,
+      //           y = _cfg$center.y;
+      //
+      //         let v = [1, 0];
+      //         let v0 = [newPoints[0].x - x, newPoints[0].y - y];
+      //         let v1 = [newPoints[1].x - x, newPoints[1].y - y];
+      //         let v2 = [newPoints[2].x - x, newPoints[2].y - y];
+      //
+      //         let startAngle = Vector2.angleTo(v, v1);
+      //         let endAngle = Vector2.angleTo(v, v2);
+      //         let r0 = Vector2.length(v0);
+      //         let r = Vector2.length(v1);
+      //
+      //         if (startAngle >= 1.5 * Math.PI) {
+      //           startAngle = startAngle - 2 * Math.PI;
+      //         }
+      //
+      //         if (endAngle >= 1.5 * Math.PI) {
+      //           endAngle = endAngle - 2 * Math.PI;
+      //         }
+      //
+      //         let lineWidth = r - r0;
+      //         let newRadius = r - lineWidth / 2;
+      //
+      //         return container.addShape('Arc', {
+      //           className: 'interval',
+      //           attrs: Util.mix({
+      //             x: x,
+      //             y: y,
+      //             startAngle: startAngle,
+      //             endAngle: endAngle,
+      //             r: newRadius,
+      //             lineWidth: lineWidth,
+      //             lineCap: 'round'
+      //           }, style)
+      //         });
+      //       }
+      //     }
+      //   });
+      //   this.initialise(Util, data);
+      // },
+      //
+      // initialise(Util, data) {
+      //   this.chart = new F2.Chart({
+      //     id: 'mountNode',
+      //     width: this.screenWidth,
+      //     pixelRatio: window.devicePixelRatio,
+      //   });
+      //   this.chart.source(this.data, {
+      //     y: {
+      //       max: 100,
+      //       min: 0
+      //     }
+      //   });
+      //   this.chart.axis(false); // hide all axis 坐标
+      //   this.chart.tooltip(false); // hide tooltip 个性化配置
+      //
+      //   // set polar coordinate 设置坐标系类型
+      //   this.chart.coord('polar', {  //极坐标系
+      //     transposed: true,
+      //     innerRadius: 0.9,
+      //     radius: 1
+      //   });
+      //
+      //   // draw a cricle 辅助弧线
+      //   this.chart.guide().arc({
+      //     start: [1, 0],
+      //     end: [1, 99.98],
+      //     top: false,
+      //     style: {
+      //       lineWidth: 15,
+      //       stroke: '#ccc'
+      //     }
+      //   });
+      //
+      //   // 辅助 html
+      //   this.chart.guide().html({
+      //     position: ['50%', '50%'],
+      //     html: '<p id="number" style="font-size: 32px;margin: 0;color: #1890ff;transform: translate(-50%, -40%);"></p>',
+      //   });
+      //   // 使用矩形或者弧形，用面积来表示大小关系的图形，一般构成柱状图、饼图等图表
+      //   this.chart.interval().position('x*y').size(15).shape('tick').animate({
+      //     appear: {
+      //       duration: 1200,
+      //       easing: 'cubicIn',
+      //       animation: function animation(shape, animateCfg) {
+      //         let startAngle = shape.attr('startAngle');
+      //         let endAngle = shape.attr('endAngle');
+      //         shape.attr('endAngle', startAngle);
+      //         shape.animate().to(Util.mix({
+      //           attrs: {
+      //             endAngle: endAngle,
+      //           }
+      //         }, animateCfg)).onUpdate(function (frame) {
+      //           if (data) {
+      //             $('#number').text(Math.floor(frame * data) + '%');
+      //           } else {
+      //             $('#number').text(0 + '%');
+      //           }
+      //         });
+      //       }
+      //     }
+      //   });
+      //   this.chart.guide().text({
+      //     top: true, // 指定 guide 是否绘制在 canvas 最上层，默认为 true, 即绘制在最上层
+      //     position: [0, 0], // 文本的起始位置，值为原始数据值，支持 callback
+      //     content: '个人业绩占比(%)', // 显示的文本内容
+      //     style: {
+      //       fill: '#666', // 文本颜色
+      //       fontSize: '12', // 文本大小
+      //       fontWeight: 'bold', // 文本粗细
+      //       rotate: 0 // 旋转角度
+      //     }, // 文本的图形样式属性
+      //     offsetX: this.screenWidth / 3, // x 方向的偏移量
+      //     offsetY: 40, // y 方向偏移量
+      //   });
+      //   this.chart.render();
+      // },
     },
   }
 </script>
@@ -389,7 +431,6 @@
       -webkit-overflow-scrolling: touch;
       background-color: $colorF;
       .mainList {
-        padding: .2rem 0 0 .32rem;
         .contract {
           color: #555555;
           div {
