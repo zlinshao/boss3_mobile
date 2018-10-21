ae000b0f45cef246ba8f2154f8c3f9b1d5559d99
linshao <601267015@qq.com>
2018-10-21 21:01:41 +0800

    weixin

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,26 @@
 <template>
   <div id="app">
-    <div class="module" v-if="loading"></div>
-    <div class="loading" v-if="loading">
-      <img src="./assets/loding1.gif">
+    <div class="module" v-if="loading">
+      <div class="formInput">
+        <div class="logos">
+          <div class="logoBig">
+            <img src="./assets/logoheyuanquan@3x.png" alt="">
+          </div>
+          <div class="logoSmall">
+            <img src="./assets/logowenzi@3x.png" alt="">
+          </div>
+        </div>
+        <div class="userInfo">
+          <span></span>
+          <input v-model="phone" placeholder="请输入手机号" type="text"/>
+        </div>
+        <div class="codeInfo">
+          <span></span><input v-model="code" placeholder="请输入验证码" type="text"/>
+          <b v-if="!loadingSms" class="codes" @click="phoneLogin">获取验证码</b>
+          <b v-if="loadingSms" class="codes">发送中({{loadingNum}})</b>
+        </div>
+        <div class="loginEnter" @click="sureLogin(phone, code)">登录</div>
+      </div>
     </div>
     <div v-if="!loading">
       <keep-alive>
@@ -13,6 +31,7 @@
 </template>
 
 <script>
+  import {Toast} from 'vant';
 
   export default {
     data() {
@@ -23,11 +42,21 @@
         showKeyboard: false,
         transitionName: '',
         loading: true,
-        token: '',
+        loadingSms: false,
+        loadingNum: 60,
+        phone: '',
+        code: '',
       };
     },
     watch: {//使用watch 监听$router的变化
       $route(to, from) {
+        if(to.path === '/'){
+          this.loading = true;
+          this.loadingSms = false;
+          this.loadingNum = 60;
+          this.phone = '';
+          this.code = '';
+        }
         //如果to索引大于from索引,判断为前进状态,反之则为后退状态
         if (to.meta.index > from.meta.index) {
           //设置动画名称
@@ -38,13 +67,13 @@
       }
     },
     mounted() {
+      console.log(this.$route.query.status);
       this.paths = this.$router.options.routes;
       this.responses();
     },
     methods: {
       responses() {
         if (navigator.userAgent == 'app/ApartMent' || navigator.userAgent.indexOf('native-ios') > -1) {
-          // if (navigator.userAgent == 'app/ApartMent') {  
           let type, token;
           if (navigator.userAgent.indexOf('native-ios') > -1) {
             token = this.$route.query.token;
@@ -54,8 +83,6 @@
             type = android.queryType();
           }
           sessionStorage.setItem('queryType', type);
-          this.loading = true;
-          // add by cj 2018-05-25
           if (type === 'exam') {
             this.$router.push({path: '/beforeExam'});
           } else if (type === 'questionnaire') {
@@ -65,14 +92,8 @@
           } else if (type === 'staffSquare') {
             this.$router.push({path: '/staffSquare'});
           }
-          // let head = {};
-          // head.token_type = "Bearer";
-          // head.access_token = android.queryToken();
-          // sessionStorage.setItem('myData', JSON.stringify(head));
           globalConfig.header.Authorization = "Bearer" + ' ' + token;
           this.$http.get(globalConfig.server + "special/special/loginInfo").then((res) => {
-            this.loading = false;
-
             let data = {};
             data.id = res.data.data.id;
             data.name = res.data.data.name;
@@ -84,15 +105,6 @@
           });
         } else {
           sessionStorage.setItem('queryType', 'ding');
-          this.loading = true;
-          this.corp();
-          // if (sessionStorage.myData !== undefined) {
-          //   let head = JSON.parse(sessionStorage.myData);
-          //   globalConfig.header.Authorization = head.token_type + ' ' + head.access_token;
-          // } else {
-          //   this.loading = true;
-          //   this.corp();
-          // }
         }
         this.$http.interceptors.response.use(function (response) {
           return response;
@@ -100,195 +112,75 @@
           if (error && error.response) {
             if (error.response.status > 499) {
               alert('服务器故障,请联系产品经理~');
-              DingTalkPC.device.notification.alert({
-                message: "服务器故障,请联系产品经理~",
-                title: "提示信息",
-                buttonName: "关闭",
-                onSuccess: function () {
-                },
-                onFail: function (err) {
-                }
-              });
-              dd.biz.navigation.close({
-                onSuccess: function (result) {
-                },
-                onFail: function (err) {
-                }
-              });
             }
           }
           return Promise.reject(error);
         });
       },
 
-      // 认证
-      corp() {
-        let that = this;
-        this.$http.get(this.urls + 'special/special/dingConfig').then((res) => {
-          let _config = res.data;
-          DingTalkPC.runtime.permission.requestAuthCode({
-            corpId: _config.corpId,
-            onSuccess: function (info) {
-              that.$http.get(that.urls + 'special/special/userInfo', {
-                params: {
-                  'code': info.code,
-                }
-              }).then((res) => {
-                if (res.data.status !== 'fail') {
-                  if (res.data !== false) {
-                    let data = {};
-                    data.id = res.data.id;
-                    data.name = res.data.name;
-                    data.avatar = res.data.avatar;
-                    data.phone = res.data.phone;
-                    data.department_name = res.data.org[0].name;
-                    data.department_id = res.data.org[0].id;
-                    sessionStorage.setItem('personal', JSON.stringify(data));
-                    globalConfig.personal = data;
-                    that.$http.post(that.address + 'oauth/token', {
-                      client_secret: globalConfig.client_secret,
-                      client_id: globalConfig.client_id,
-                      grant_type: 'password',
-                      username: res.data.phone,
-                      password: res.data.code,
-                    }).then((res) => {
-                      let head = res.data.data;
-                      globalConfig.header.Authorization = head.token_type + ' ' + head.access_token;
-                      that.loading = false;
-                    });
-                  } else {
-                    setTimeout(() => {
-                      DingTalkPC.device.notification.alert({
-                        message: "请求超时请稍后再试",
-                        title: "提示信息",
-                        buttonName: "关闭",
-                        onSuccess: function () {
-                        },
-                        onFail: function (err) {
-                        }
-                      });
-                      dd.biz.navigation.close({
-                        onSuccess: function (result) {
-                        },
-                        onFail: function (err) {
-                        }
-                      });
-                    }, 3000);
-                  }
-                } else {
-                  DingTalkPC.device.notification.alert({
-                    message: "读取信息失败，稍后再试！",
-                    title: "提示信息",
-                    buttonName: "关闭",
-                    onSuccess: function () {
-                    },
-                    onFail: function (err) {
-                    }
-                  });
-                  dd.biz.navigation.close({
-                    onSuccess: function (result) {
-                    },
-                    onFail: function (err) {
-                    }
-                  });
-                }
-              })
-            },
-            onFail: function (err) {
-              DingTalkPC.device.notification.alert({
-                message: "您不在系统内，请联系管理员添加！！",
-                title: "提示信息",
-                buttonName: "关闭",
-                onSuccess: function () {
-                },
-                onFail: function (err) {
-                }
-              });
+      phoneLogin() {
+        this.$http.post(globalConfig.server_token + 'api/v1/sms', {
+          phone: this.phone,
+        }).then((res) => {
+          let msg = res.data.message;
+          if (res.data.status === 'success') {
+            this.countDown();
+            this.loadingSms = true;
+            Toast.success(msg);
+          } else {
+            if (typeof msg !== 'string') {
+              Toast(res.data.message.phone[0]);
+            } else {
+              Toast(res.data.message);
             }
-          });
-
-          dd.ready(function () {
-            dd.runtime.permission.requestAuthCode({
-              corpId: _config.corpId,
-              onSuccess: function (info) {
-                that.$http.get(that.urls + 'special/special/userInfo', {
-                  params: {
-                    'code': info.code,
-                  }
-                }).then((res) => {
-                  if (res.data.status !== 'fail') {
-                    if (res.data !== false) {
-                      let data = {};
-                      data.id = res.data.id;
-                      data.name = res.data.name;
-                      data.avatar = res.data.avatar;
-                      data.phone = res.data.phone;
-                      data.department_name = res.data.org[0].name;
-                      data.department_id = res.data.org[0].id;
-                      sessionStorage.setItem('personal', JSON.stringify(data));
-                      globalConfig.personal = data;
-                      that.$http.post(that.address + 'oauth/token', {
-                        client_secret: globalConfig.client_secret,
-                        client_id: globalConfig.client_id,
-                        grant_type: 'password',
-                        username: res.data.phone,
-                        password: res.data.code,
-                      }).then((res) => {
-                        let head = res.data.data;
-                        globalConfig.header.Authorization = head.token_type + ' ' + head.access_token;
-                        that.loading = false;
-                      });
-                    } else {
-                      setTimeout(() => {
-                        alert('请求超时请稍后再试');
-                        dd.biz.navigation.close({
-                          onSuccess: function (result) {
-                          },
-                          onFail: function (err) {
-                          }
-                        });
-                      }, 3000);
-                    }
-                  } else {
-                    alert('读取信息失败，稍后再试！');
-                    dd.biz.navigation.close({
-                      onSuccess: function (result) {
-                      },
-                      onFail: function (err) {
-                      }
-                    });
-                  }
-                })
-              },
-              onFail: function (err) {
-                alert('您不在系统内，请联系管理员添加！！');
-                dd.biz.navigation.close({
-                  onSuccess: function (result) {
-                  },
-                  onFail: function (err) {
-                  }
-                });
-              }
-            });
-            // 钉钉头部右侧
-            dd.biz.navigation.setRight({
-              show: false,
-              onSuccess: function (result) {
-              },
-              onFail: function (err) {
-              }
-            });
-          });
-          dd.error(function (err) {
-            alert('dd error: ' + JSON.stringify(err));
-          });
+          }
         })
       },
 
+      countDown() {
+        new Promise((resolve, reject) => {
+          let interval = setInterval(() => {
+            if (this.loadingNum > 0) {
+              this.loadingNum--;
+            }
+            if (this.loadingNum === 0) {
+              this.loadingNum = 60;
+              clearInterval(interval);
+              this.loadingSms = false;
+            }
+          }, 1000)
+        })
+      },
+
+      sureLogin(a, b) {
+        this.$http.post(globalConfig.server_token + 'oauth/token', {
+          client_secret: globalConfig.client_secret,
+          client_id: globalConfig.client_id,
+          grant_type: 'password',
+          username: a,
+          password: b,
+        }).then((res) => {
+          let head = res.data.data;
+          globalConfig.header.Authorization = head.token_type + ' ' + head.access_token;
+
+          this.$http.get(globalConfig.server + "special/special/loginInfo").then((res) => {
+            let data = {};
+            data.id = res.data.data.id;
+            data.name = res.data.data.name;
+            data.avatar = res.data.data.avatar;
+            data.phone = res.data.data.phone;
+            data.department_name = res.data.data.org[0].name;
+            data.department_id = res.data.data.org[0].id;
+            sessionStorage.setItem('personal', JSON.stringify(data));
+            globalConfig.personal = data;
+            this.loading = false;
+            Toast.success('登录成功');
+          });
+        });
+      },
       onInput(key) {
         this.value = (this.value + key).slice(0, 6);
       },
-
       onDelete() {
         this.value = this.value.slice(0, this.value.length - 1);
       }
@@ -298,24 +190,95 @@
 
 <style lang="scss">
   #app {
+    @mixin flex {
+      display: flex;
+      display: -webkit-flex;
+      align-items: center;
+    }
+    @mixin radius {
+      -webkit-border-radius: .08rem;
+      -moz-border-radius: .08rem;
+      border-radius: .08rem;
+    }
     height: 100%;
-    .module, .loading {
-      position: fixed;
-    }
-
     .module {
-      left: 0;
-      right: 0;
-      top: 0;
-      bottom: 0;
-      background: #f1f1f1;
-    }
-
-    .loading {
-      top: 50%;
-      left: 50%;
-      transform: translate(-50%, -50%);
-      z-index: 1;
+      width: 100%;
+      height: 100%;
+      background-image: url("http://s.lejias.cn/group_2@3x.png");
+      background-size: 100% 100%;
+      @include flex();
+      justify-content: center;
+      .formInput {
+        background-color: #fff;
+        @include radius();
+        padding: 0 .36rem;
+        .userInfo, .codeInfo {
+          height: 1.3rem;
+          border-bottom: 1px solid #e0e0e0;
+        }
+        .userInfo {
+          @include flex();
+          span {
+            height: .5rem;
+            background-image: url("./assets/shouji@3x.png");
+          }
+        }
+        .codeInfo {
+          @include flex();
+          .codes {
+            @include radius();
+            width: 1.6rem;
+            height: .6rem;
+            line-height: .6rem;
+            color: #58d788;
+            font-size: .24rem;
+            margin-left: .2rem;
+            background-color: rgba(88, 215, 136, 0.20);
+            text-align: center;
+          }
+          span {
+            height: .45rem;
+            background-image: url("./assets/shoujiyanzhengma@3x.png");
+          }
+        }
+        span {
+          margin-right: .12rem;
+          width: .4rem;
+          background-size: 100% 100%;
+        }
+        .logoBig {
+          margin-top: -1.2rem;
+          text-align: center;
+          img {
+            width: 2rem;
+            height: 2rem;
+          }
+        }
+        .logoSmall {
+          text-align: center;
+          margin: .2rem 0 .5rem;
+          img {
+            width: 1.4rem;
+            height: .8rem;
+          }
+        }
+      }
+      input {
+        width: 3rem;
+        @include radius();
+        border: none;
+        @include radius();
+        padding: .12rem .24rem;
+      }
+      .loginEnter {
+        margin: .6rem 0 1rem;
+        background-image: linear-gradient(-135deg, #ffd54f 0%, #feb300 100%);
+        box-shadow: 0 2px 20px 0 rgba(255, 179, 0, 0.30);
+        color: #FFFFFF;
+        text-align: center;
+        padding: .2rem 0;
+        @include radius();
+      }
     }
   }
 
--- src/fun.config.js
+++ src/fun.config.js
@@ -1,6 +1,6 @@
 export default {
   install(Vue, options) {
-    Vue.prototype.routerIndex = function (url, house, id) {
+    Vue.prototype.routerIndex = function (url, house) {
       let that = this;
       document.addEventListener('backbutton', function (e) {
         e.preventDefault();
@@ -9,16 +9,15 @@ export default {
         } else if (url !== '' && house !== 'house') {
           that.$router.push({path: url, query: {tops: ''}});
         } else if (house === 'close') {
-          dd.biz.navigation.close({
-            onSuccess: function (result) {
-            },
-            onFail: function (err) {
-            }
-          });
+
         } else {
           that.$router.push({path: '/index'});
         }
       });
+    };
+    Vue.prototype.ddRent = function (url, house) {
+      let that = this;
+      this.routerIndex(url, house);
     };
     Vue.prototype.routLink = function (path, params) {
       if (path === 'back') {
@@ -27,6 +26,14 @@ export default {
         this.$router.push({path: path, query: params});
       }
     };
+    Vue.prototype.routerTo = function (url, id) {
+      let that = this;
+      document.addEventListener('backbutton', function (e) {
+        e.preventDefault();
+        that.$router.push({path: url, query: {ids: id}});
+      });
+    };
+    // 算钱
     Vue.prototype.countMoney = function (form) {
       let sum1 = form.front_money;
       let sum2 = form.deposit_payed;
@@ -35,47 +42,6 @@ export default {
       sum2 = sum2 ? sum2 : 0;
       sum3 = sum3 ? sum3 : 0;
       return Number(sum1) + Number(sum2) + Number(sum3);
-    };
-    Vue.prototype.ddRent = function (url, house) {
-      let that = this;
-      dd.biz.navigation.setLeft({
-        control: true,//是否控制点击事件，true 控制，false 不控制， 默认false
-        onSuccess: function (result) {
-          if (url !== '' && house === 'house') {
-            that.$router.push({path: url});
-          } else if (url !== '' && house !== 'house') {
-            that.$router.push({path: url, query: {tops: ''}});
-          } else if (house === 'close') {
-            dd.biz.navigation.close({
-              onSuccess: function (result) {
-              },
-              onFail: function (err) {
-              }
-            });
-          } else {
-            that.$router.push({path: '/index'});
-          }
-        },
-        onFail: function (err) {
-        }
-      });
-    };
-
-    Vue.prototype.routerTo = function (url, id, val) {
-      let that = this;
-      document.addEventListener('backbutton', function (e) {
-        e.preventDefault();
-        that.$router.push({path: url, query: {ids: id}});
-      });
-
-      dd.biz.navigation.setLeft({
-        control: true,//是否控制点击事件，true 控制，false 不控制， 默认false
-        onSuccess: function () {
-          that.$router.push({path: url, query: {ids: id}});
-        },
-        onFail: function (err) {
-        }
-      });
     };
     // 数组去空字符串
     Vue.prototype.filter_array = function (array) {
@@ -114,8 +80,7 @@ export default {
     };
 
     Vue.prototype.stick = function () {
-      document.getElementById('main').scrollTop = document.getElementById('main').scrollHeight;
-      // console.log(document.getElementsByTagName('.main')[0].scrollHeight);
+      window.scrollTo(0, document.getElementById('main').scrollHeight);
     };
     Vue.prototype.valueLength = function (item, val) {
       return item.slice(0, val);
@@ -156,169 +121,23 @@ export default {
           return '请删除上传失败的文件并重新上传!';
       }
     };
-
+    // that.$http.get(globalConfig.server + "special/special/loginInfo").then((res) => {
+    //   let data = {};
+    //   data.id = res.data.data.id;
+    //   data.name = res.data.data.name;
+    //   data.avatar = res.data.data.avatar;
+    //   data.phone = res.data.data.phone;
+    //   data.department_name = res.data.data.org[0].name;
+    //   data.department_id = res.data.data.org[0].id;
+    //   sessionStorage.setItem('personal', JSON.stringify(data));
+    //   globalConfig.personal = data;
+    //   resolve(true);
+    // });
     Vue.prototype.personalGet = function () {
       let that = this;
       return new Promise((resolve, reject) => {
-        that.$http.get(globalConfig.server + 'special/special/dingConfig').then((res) => {
-          let _config = res.data;
-          DingTalkPC.runtime.permission.requestAuthCode({
-            corpId: _config.corpId,
-            onSuccess: function (info) {
-              that.$http.get(globalConfig.server + 'special/special/userInfo', {
-                params: {
-                  'code': info.code,
-                }
-              }).then((res) => {
-                if (res.data.status !== 'fail') {
-                  if (res.data !== false) {
-                    let data = {};
-                    data.id = res.data.id;
-                    data.name = res.data.name;
-                    data.avatar = res.data.avatar;
-                    data.phone = res.data.phone;
-                    data.department_name = res.data.org[0].name;
-                    data.department_id = res.data.org[0].id;
-                    sessionStorage.setItem('personal', JSON.stringify(data));
-                    globalConfig.personal = data;
-                    that.$http.post(globalConfig.attestation + 'oauth/token', {
-                      client_secret: globalConfig.client_secret,
-                      client_id: globalConfig.client_id,
-                      grant_type: 'password',
-                      username: res.data.phone,
-                      password: res.data.code,
-                    }).then((res) => {
-                      let head = res.data.data;
-                      globalConfig.header.Authorization = head.token_type + ' ' + head.access_token;
-                      resolve(true);
-                    });
-                  } else {
-                    setTimeout(() => {
-                      DingTalkPC.device.notification.alert({
-                        message: "请求超时请稍后再试",
-                        title: "提示信息",
-                        buttonName: "关闭",
-                        onSuccess: function () {
-                        },
-                        onFail: function (err) {
-                        }
-                      });
-                      dd.biz.navigation.close({
-                        onSuccess: function (result) {
-                        },
-                        onFail: function (err) {
-                        }
-                      });
-                    }, 3000);
-                  }
-                } else {
-                  DingTalkPC.device.notification.alert({
-                    message: "读取信息失败，稍后再试！",
-                    title: "提示信息",
-                    buttonName: "关闭",
-                    onSuccess: function () {
-                    },
-                    onFail: function (err) {
-                    }
-                  });
-                  dd.biz.navigation.close({
-                    onSuccess: function (result) {
-                    },
-                    onFail: function (err) {
-                    }
-                  });
-                }
-              })
-            },
-            onFail: function (err) {
-              DingTalkPC.device.notification.alert({
-                message: "您不在系统内，请联系管理员添加！！",
-                title: "提示信息",
-                buttonName: "关闭",
-                onSuccess: function () {
-                },
-                onFail: function (err) {
-                }
-              });
-            }
-          });
-
-          dd.ready(function () {
-            dd.runtime.permission.requestAuthCode({
-              corpId: _config.corpId,
-              onSuccess: function (info) {
-                that.$http.get(globalConfig.server + 'special/special/userInfo', {
-                  params: {
-                    'code': info.code,
-                  }
-                }).then((res) => {
-                  if (res.data.status !== 'fail') {
-                    if (res.data !== false) {
-                      let data = {};
-                      data.id = res.data.id;
-                      data.name = res.data.name;
-                      data.avatar = res.data.avatar;
-                      data.phone = res.data.phone;
-                      data.department_name = res.data.org[0].name;
-                      data.department_id = res.data.org[0].id;
-                      sessionStorage.setItem('personal', JSON.stringify(data));
-                      globalConfig.personal = data;
-                      that.$http.post(globalConfig.attestation + 'oauth/token', {
-                        client_secret: globalConfig.client_secret,
-                        client_id: globalConfig.client_id,
-                        grant_type: 'password',
-                        username: res.data.phone,
-                        password: res.data.code,
-                      }).then((res) => {
-                        let head = res.data.data;
-                        globalConfig.header.Authorization = head.token_type + ' ' + head.access_token;
-                        resolve(true);
-                      });
-                    } else {
-                      setTimeout(() => {
-                        alert('请求超时请稍后再试');
-                        dd.biz.navigation.close({
-                          onSuccess: function (result) {
-                          },
-                          onFail: function (err) {
-                          }
-                        });
-                      }, 3000);
-                    }
-                  } else {
-                    alert('读取信息失败，稍后再试！');
-                    dd.biz.navigation.close({
-                      onSuccess: function (result) {
-                      },
-                      onFail: function (err) {
-                      }
-                    });
-                  }
-                })
-              },
-              onFail: function (err) {
-                alert('您不在系统内，请联系管理员添加！！');
-                dd.biz.navigation.close({
-                  onSuccess: function (result) {
-                  },
-                  onFail: function (err) {
-                  }
-                });
-              }
-            });
-            // 钉钉头部右侧
-            dd.biz.navigation.setRight({
-              show: false,
-              onSuccess: function (result) {
-              },
-              onFail: function (err) {
-              }
-            });
-          });
-          dd.error(function (err) {
-            alert('dd error: ' + JSON.stringify(err));
-          });
-        });
+        that.$router.push({path: '/'});
+        resolve(false);
       });
     };
   }
