c1cf966bb52ede5d75fe627d60001d56e556a5c6
linshao <601267015@qq.com>
2018-10-21 21:25:33 +0800

    weixin

--- src/boss.config.js
+++ src/boss.config.js
@@ -1,9 +1,7 @@
 globalConfig = {
   server_token:"http://test.boss-support.lejias.cn/",                 //登录接口
-  appId:'dingoabeclxxwagukzyegm',
   server: 'http://test.v3.api.boss.lejias.cn/',                       //接口地址
   server_user: 'http://test.boss-support.lejias.cn/api/s1/',          //老大接口地址
-  attestation: 'http://test.boss-support.lejias.cn/',                 //认证接口
   env: 'dev',       //用户名
   domain: 'http://static.lejias.cn',
   personal: {},
@@ -11,6 +9,5 @@ globalConfig = {
   client_id: '2',
   header: {
     'Accept': 'application/json',
-    'Authorization': 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImp0aSI6IjFjMmJmOTg4NjY0NjZhNGQyNmZkNjJjYTM4ZDc2MzJkZTI3ODdiMzE5ZDMzZWRiZjc5OTkzOTQ1NzkyNTIxZjU1NjY1YThlYTI2MjE2ZjcxIn0.eyJhdWQiOiIyIiwianRpIjoiMWMyYmY5ODg2NjQ2NmE0ZDI2ZmQ2MmNhMzhkNzYzMmRlMjc4N2IzMTlkMzNlZGJmNzk5OTM5NDU3OTI1MjFmNTU2NjVhOGVhMjYyMTZmNzEiLCJpYXQiOjE1NDAxMTU5ODcsIm5iZiI6MTU0MDExNTk4NywiZXhwIjoxNTQwOTc5OTg3LCJzdWIiOiI2OSIsInNjb3BlcyI6W119.Fr7FwsK2HsmJ9hNlprhOKUVBthmJyB1MN0OS93v_3ZWyUegzpCcvJIAK4CkGY2tZ_W2m9-0whg139xQ0LWLhBUYdce-kAXH8LCUmRFDZHJLnHYNVMOPT_fFgGO-GW0myE21KbhuYozEqE000Asq9hKvy9KyvIu0Gt6zXtGoioTaVMdLLtaSOx0dLSHjJ1UREhhZ004NDJjzfvbz22i18TqfOiMdkPtRQtb85k8c4r_qzNQmJ1E51J2pgYmwIyfyyo1uQdOSeLNv6pQeXOTSn_USJQTKdakcxtwCTKLqF5g18fJHOjjILyIgg8CtW19isByyM4gjZ3h-lMoP7dBYBmCLwN4mSWHRSjuy6gSYYylwnIdZ1PVhe11eLhKGDwTQwX8iXPpqi72VHRMXbqjfo0EAkATBt3id0Qc4elhQSu9Npny8u5faMUkFGGNxlKUvvV8Su_PzsNddfDZgjBl6KOUSMbX3MhygIyqzNcKI7xWIOEOraoVZSlle4IeS9UyUL--AAmkg88y0RXJY1eWPnNJZ4bade-Sly4srUQlKho9_3j4rGpIu63-bJczU11FMIN84v1Rfd2LTlzAAnbc58F4g9aC32_KMbB97gXCQWV-saGpdK73FSvls3v8rcVCbOiwRDIqlDuf_7SuRPy93DjQV8ZZQpxFkK3q79ci34GcQ'
   }
 };
