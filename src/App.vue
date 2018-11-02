264cbb33fdb7809bb422ea152d32cd635f1b46b0
linshao <601267015@qq.com>
2018-11-02 13:46:57 +0800

    fff

--- src/App.vue
+++ src/App.vue
@@ -91,20 +91,18 @@
           if (error && error.response) {
             if (error.response.status > 499) {
               alert('服务器故障,请联系产品经理~');
-              DingTalkPC.device.notification.alert({
-                message: "服务器故障,请联系产品经理~",
-                title: "提示信息",
-                buttonName: "关闭",
-              });
-              that.closeDD();
             }
           }
           return Promise.reject(error);
         });
       },
       prevent() {
+        alert(11111111)
         this.$http.get('https://qyapi.weixin.qq.com/cgi-bin/gettoken?corpid=ww469e1dbe19ea6189&corpsecret=f7B6EMEeyXI_z7v7IbmWD-5zzO6HZyEKuOYz16kNoJY').then(res => {
+          alert(222222222)
           this.token = res.data;
+        }).catch(err => {
+          alert(3333333333)
         })
       },
       onInput(key) {
