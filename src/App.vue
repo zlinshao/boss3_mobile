2c0a7605caa48a8c4d9a96ee8a972796dc48fff0
linshao <601267015@qq.com>
2018-11-09 18:31:30 +0800

    fff

--- src/App.vue
+++ src/App.vue
@@ -92,12 +92,6 @@
           if (error && error.response) {
             if (error.response.status > 499) {
               alert('服务器故障,请联系产品经理~');
-              DingTalkPC.device.notification.alert({
-                message: "服务器故障,请联系产品经理~",
-                title: "提示信息",
-                buttonName: "关闭",
-              });
-              that.closeDD();
             }
           }
           return Promise.reject(error);
