9cb9723aa1851a9dc061c73e134ce3ccd0090e23
linshao <601267015@qq.com>
2018-11-10 10:08:36 +0800

    fff

--- src/App.vue
+++ src/App.vue
@@ -47,13 +47,13 @@
     mounted() {
       this.paths = this.$router.options.routes;
       this.responses();
-      alert(this.isWeiXin);
+      this.isWeiXin();
     },
     methods: {
       isWeiXin() {
         //window.navigator.userAgent属性包含了浏览器类型、版本、操作系统类型、浏览器引擎类型等信息，这个属性可以用来判断浏览器类型
-        let ua = window.navigator.userAgent.toLowerCase();
-        alert(ua);
+        let ua = navigator.userAgent.toLowerCase();
+        alert(navigator.userAgent);
         //通过正则表达式匹配ua中是否含有MicroMessenger字符串
         return ua.match(/MicroMessenger/i) === 'micromessenger';
       },
