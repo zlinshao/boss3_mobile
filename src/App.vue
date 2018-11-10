7be2e98d98c9005b07808279fba5662396a9d024
linshao <601267015@qq.com>
2018-11-10 10:13:32 +0800

    fff

--- src/App.vue
+++ src/App.vue
@@ -54,9 +54,8 @@
       isWeiXin() {
         //window.navigator.userAgent属性包含了浏览器类型、版本、操作系统类型、浏览器引擎类型等信息，这个属性可以用来判断浏览器类型
         let ua = navigator.userAgent.toLowerCase();
-        alert(navigator.userAgent);
         //通过正则表达式匹配ua中是否含有MicroMessenger字符串
-        return ua.match(/MicroMessenger/i) === 'micromessenger';
+        return ua.match(/micromessenger/i) === 'micromessenger';
       },
       responses() {
         if (navigator.userAgent == 'app/ApartMent' || navigator.userAgent.indexOf('native-ios') > -1) {
