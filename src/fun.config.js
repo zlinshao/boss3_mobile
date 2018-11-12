40b7124a026ff6576fdfa5566bbf5b873f4e16d4
linshao <601267015@qq.com>
2018-11-12 17:59:05 +0800

    ff

--- src/fun.config.js
+++ src/fun.config.js
@@ -182,7 +182,6 @@ export default {
       return new Promise((resolve, reject) => {
         val.timestamp = new Date().getTime();
         val.nonceStr = md5(val.corpid + val.timestamp);
-        alert(JSON.stringify(val));
         this.$http.get(globalConfig.server + 'organization/wework-bulletin', {
           params: val,
         }).then(res => {
@@ -196,6 +195,7 @@ export default {
             signature: res.data.data.signature,// 必填，签名，见附录1
             jsApiList: ['onHistoryBack', 'hideOptionMenu'] // 必填，需要使用的JS接口列表，所有JS接口列表见附录2
           });
+          alert(JSON.stringify(val));
           alert(JSON.stringify({
             beta: true,// 必须这么写，否则wx.invoke调用形式的jsapi会有问题
             debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
@@ -204,7 +204,7 @@ export default {
             nonceStr: val.nonceStr, // 必填，生成签名的随机串
             signature: res.data.data.signature,// 必填，签名，见附录1
             jsApiList: ['onHistoryBack', 'hideOptionMenu'] // 必填，需要使用的JS接口列表，所有JS接口列表见附录2
-          }), 1, JSON.stringify(val));
+          }));
           resolve(true);
         }).catch(err => {
           resolve(false);
