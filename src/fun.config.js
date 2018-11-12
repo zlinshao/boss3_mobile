38627caa181de7ff42316af01b93def4fa0c8b8e
linshao <601267015@qq.com>
2018-11-12 15:48:33 +0800

    nonceStr

--- src/fun.config.js
+++ src/fun.config.js
@@ -179,6 +179,7 @@ export default {
     // 企业微信
     Vue.prototype.weiChatAuth = function (val) {
       return new Promise((resolve, reject) => {
+        alert(JSON.stringify(val));
         this.$http.get(globalConfig.server + 'organization/wework-bulletin', {
           params: val,
         }).then(res => {
@@ -186,6 +187,10 @@ export default {
           let token = res.data.data;
           let time = new Date().getTime();
           let nonceStr = md5(appId + token + time);
+          alert(appId);
+          alert(token);
+          alert(time);
+          alert(nonceStr);
           wx.config({
             beta: true,// 必须这么写，否则wx.invoke调用形式的jsapi会有问题
             debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
