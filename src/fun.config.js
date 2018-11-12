e3cff005ae5bca65ce1a935d9700eded1b63489c
linshao <601267015@qq.com>
2018-11-12 16:45:12 +0800

    ff

--- src/fun.config.js
+++ src/fun.config.js
@@ -192,18 +192,18 @@ export default {
             appId: appId, // 必填，企业微信的corpID
             timestamp: time, // 必填，生成签名的时间戳
             nonceStr: nonceStr, // 必填，生成签名的随机串
-            signature: ticket,// 必填，签名，见附录1
+            signature: 'HoagFKDcsGMVCIY2vOjf9iYBsDIkCFzKneUnenO0Pgci9axRI9NF4H8emuDlelOXA9QAJ2whYQxvF9tIH-pD-Q',// 必填，签名，见附录1
             jsApiList: ['onHistoryBack','hideOptionMenu'] // 必填，需要使用的JS接口列表，所有JS接口列表见附录2
           });
-          alert(JSON.stringify({
-            beta: true,// 必须这么写，否则wx.invoke调用形式的jsapi会有问题
-            debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
-            appId: appId, // 必填，企业微信的corpID
-            timestamp: time, // 必填，生成签名的时间戳
-            nonceStr: nonceStr, // 必填，生成签名的随机串
-            signature: ticket,// 必填，签名，见附录1
-            jsApiList: ['onHistoryBack','hideOptionMenu'] // 必填，需要使用的JS接口列表，所有JS接口列表见附录2
-          }));
+          // alert(JSON.stringify({
+          //   beta: true,// 必须这么写，否则wx.invoke调用形式的jsapi会有问题
+          //   debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
+          //   appId: appId, // 必填，企业微信的corpID
+          //   timestamp: time, // 必填，生成签名的时间戳
+          //   nonceStr: nonceStr, // 必填，生成签名的随机串
+          //   signature: 'HoagFKDcsGMVCIY2vOjf9iYBsDIkCFzKneUnenO0Pgci9axRI9NF4H8emuDlelOXA9QAJ2whYQxvF9tIH-pD-Q',// 必填，签名，见附录1
+          //   jsApiList: ['onHistoryBack','hideOptionMenu'] // 必填，需要使用的JS接口列表，所有JS接口列表见附录2
+          // }));
           resolve(true);
         }).catch(err => {
           resolve(false);
