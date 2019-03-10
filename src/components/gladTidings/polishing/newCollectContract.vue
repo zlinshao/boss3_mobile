18155854771b0f0a773061ee625e830e8cf226d5
kyle <zhengxu@zmit.cn>
2019-03-10 09:09:54 +0800

    UPDATE

--- src/components/gladTidings/polishing/newCollectContract.vue
+++ src/components/gladTidings/polishing/newCollectContract.vue
@@ -656,7 +656,7 @@
       this.name = name || '';
       this.idcard = idcard || '';
       this.phone = phone || '';
-      this.fadada_user_id = userid || '';
+      this.fadada_user_id = 'AD4D9D5FFA9C4B0500DE29F99DE1CF5DB8EBB9DA925BA67D9860FC575697ADDAEC983C8F76C7F49A1A3B0D8593162E5C';
     }
   }
 
