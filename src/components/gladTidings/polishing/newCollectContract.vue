723a5f7547c3ab22b292290918d76c012d4b6ade
kyle <zhengxu@zmit.cn>
2019-03-18 14:37:40 +0800

    修改收房合同

--- src/components/gladTidings/polishing/newCollectContract.vue
+++ src/components/gladTidings/polishing/newCollectContract.vue
@@ -495,7 +495,15 @@
           </van-field>
         </div>
 
-
+        <van-field
+          v-model="form.account_name"
+          label="开户名"
+          type="text"
+          placeholder="请填写开户名"
+          icon="clear"
+          @click-icon="form.account_name = ''"
+          required>
+        </van-field>
         <div class="month">
           <van-field
             v-model="form.account"
@@ -527,15 +535,7 @@
           icon="clear"
           @click-icon="form.subbranch = ''">
         </van-field>
-        <van-field
-          v-model="form.account_name"
-          label="开户名"
-          type="text"
-          placeholder="请填写开户名"
-          icon="clear"
-          @click-icon="form.account_name = ''"
-          required>
-        </van-field>
+
         <van-field
           v-model="form.relationship"
           label="关系"
@@ -1235,7 +1235,7 @@
         this.form.cookie = per.session_id;
       },
       accountBank(val) {
-        this.$http.get(this.urls + 'bulletin/helper/bankname?card=' + val).then((res) => {
+        this.$http.get(this.urls + 'bulletin/helper/bankname?card=' + val+"&owner="+this.form.account_name).then((res) => {
           if (res.data.code === '51110') {
             this.form.bank = res.data.data;
           }
@@ -1666,6 +1666,7 @@
         }
       },
       changeContractData(draft) {
+        this.form.contract_id = draft.contract_id;
         this.form.purchase_way = 509;
         this.form.house = {id: '', name: ''};
         this.form.house.id = draft.house.id;
@@ -1813,7 +1814,6 @@
         this.form.allowed_add_to = draft.allowed_add_to;
         this.showForm.canAddThingTxt = this.getNameForIndex(this.canAddThings, this.form.allowed_add_to);
         this.showForm.choosedRemarks = this.getListFromList(this.remarks, draft.other_rule);
-        this.form.contract_id = draft.contract_id;
         this.changeContracts();
 
       },
